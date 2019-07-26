6690f79997a3818d0a480feae339a8aba40d9f28
lone-echo <48450587+lone-echo@users.noreply.github.com>
2019-07-26 09:48:05 +0100

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ let headers;
 let timeout;
 const notified = [];
 
-const studiesURL = 'https://www.prolific.ac/api/v1/studies/?current=1';
+const studiesURL = 'https://www.prolific.co/api/v1/studies/?current=1';
 
 const toMoney = (n) => (n / 100).toFixed(2);
 
